919ab240fdb1f2622ba9e61bcb2f0205c5fc267d
PARKSANGWOOK <sangwookp9591@gmail.com>
2024-05-16 15:53:41 +0900

    Feat : 청구금액 계산을위한 amountfor 함수 생성

--- ex_1/sample.js
+++ ex_1/sample.js
@@ -53,4 +53,27 @@ function statement(invoice, plays) {
     return result;
 }
 
+function amountFor(perf, play) {
+    let thisAmount = 0;
+
+    switch (play.type) {
+        case 'tragedy':
+            thisAmount = 40000;
+            if (perf.audience > 30) {
+                thisAmount += 1000 * (perf.audience - 30);
+            }
+            break;
+        case 'comedy':
+            thisAmount = 30000;
+            if (perf.audience > 20) {
+                thisAmount += 10000 + 500 * (perf.audience - 20);
+            }
+            thisAmount += 300 * perf.audience;
+            break;
+        default:
+            throw new Error(`unknown type: ${play.type}`);
+    }
+    return thisAmount; //함수 안에서 값이 변경되는 변수 반환.
+}
+
 console.log('result : ', statement(invoices[0], plays));
