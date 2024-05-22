359dc47e5345b2c5d4319723ad95c8bec4bacd91
PARKSANGWOOK <sangwookp9591@gmail.com>
2024-05-22 15:39:22 +0900

    Refactor : 함수들을 계산기로 옮기기

--- ex_1/createStatementData.js
+++ ex_1/createStatementData.js
@@ -1,3 +1,41 @@
+class PerformanceCalculator {
+    constructor(aPerformance, aPlay) {
+        this.performance = aPerformance;
+        this.play = aPlay;
+    }
+
+    get amount() {
+        let result = 0;
+
+        switch (this.play.type) {
+            case 'tragedy':
+                result = 40000;
+                if (this.performance.audience > 30) {
+                    result += 1000 * (this.performance.audience - 30);
+                }
+                break;
+            case 'comedy':
+                result = 30000;
+                if (this.performance.audience > 20) {
+                    result += 10000 + 500 * (this.performance.audience - 20);
+                }
+                result += 300 * this.performance.audience;
+                break;
+            default:
+                throw new Error(`unknown type: ${this.play.type}`);
+        }
+        return result; //함수 안에서 값이 변경되는 변수 반환.
+    }
+
+    get volumeCredits() {
+        let result = 0;
+        result += Math.max(this.performance.audience - 30, 0);
+        if ('comedy' === this.play.type) result += Math.floor(this.performance.audience / 5);
+
+        return result;
+    }
+}
+
 function createStatementData(invoice, plays) {
     const statementData = {};
 
@@ -7,11 +45,12 @@ function createStatementData(invoice, plays) {
     statementData.totalVolumeCredits = totalVolumeCredits(statementData);
     return statementData; // 중간 데이터 구조를 인수로 전달
     function enrichPerformance(aPerformance) {
+        const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
         const result = Object.assign({}, aPerformance); //얕은 복사 수행
         //첫 레벨만 복사해서 가져옴.
-        result.play = playFor(result);
-        result.amount = amountFor(result);
-        result.volumeCredits = volumeCreditsFor(result);
+        result.play = calculator.play;
+        result.amount = calculator.amount;
+        result.volumeCredits = calculator.volumeCredits;
         return result;
     }
 
@@ -20,28 +59,9 @@ function createStatementData(invoice, plays) {
         return plays[aPerformance.playID];
     }
 
-    function amountFor(aPerformance) {
-        let result = 0;
-
-        switch (aPerformance.play.type) {
-            case 'tragedy':
-                result = 40000;
-                if (aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30);
-                }
-                break;
-            case 'comedy':
-                result = 30000;
-                if (aPerformance.audience > 20) {
-                    result += 10000 + 500 * (aPerformance.audience - 20);
-                }
-                result += 300 * aPerformance.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${aPerformance.play.type}`);
-        }
-        return result; //함수 안에서 값이 변경되는 변수 반환.
-    }
+    // function amountFor(aPerformance) {
+    //     return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
+    // }
     //volume creadits 구하는 함수
     function volumeCreditsFor(aPerformance) {
         let result = 0;
